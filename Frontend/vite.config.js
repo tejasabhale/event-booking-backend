50b82cd0ba3e95f0bcf6071b0357b72ae690b7f2
Tejas Abhale <abhaletejas2767.com>
2026-05-14 23:00:32 +0530

    feat: refactor vite.config.js to load environment variables for backend URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.BACKEND_URL || "http://localhost:8000",
-        changeOrigin: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.BACKEND_URL || "http://localhost:8000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  });
+};
